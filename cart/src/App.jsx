3e75d73b838ce323cb4d3ddcaba51975ae296ebe
cyroalves <cyro96@hotmail.com>
2022-09-30 22:08:13 -0300

    feat: cart, home, addtocart, pdp and server

---
+++ cart/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Footer from "home/Footer";
+import Header from "home/Header";
+
+import "remixicon/fonts/remixicon.css";
+import "./index.scss";
+import CartContent from "./CartContent";
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      <CartContent />
+    </div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ pdp/webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://localhost:3001/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env','@babel/preset-react'] },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "pdp",
+      filename: "remoteEntry.js",
+      remotes: { 
+        home:"home@http://localhost:3000/remoteEntry.js",
+        pdp:"pqp@http://localhost:3001/remoteEntry.js",
+        cart:"cart@http://localhost:3002/remoteEntry.js",
+        addtocart:"addtocart@http://localhost:3003/remoteEntry.js",
+      },
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
